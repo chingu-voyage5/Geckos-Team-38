4fb376812a229f5eaeb1e086a933f75b3d5fe90c
Harshil Agrawal <ghagrawal17@gmail.com>
2018-06-25 15:08:55 +0530

    merged prettier from dev branch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,13 +10,4 @@ ReactDOM.render(
         <App />
     </BrowserRouter>    
         , document.getElementById('root'));
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> development
 registerServiceWorker();
