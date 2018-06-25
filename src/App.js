1a196c184269e7e7cb75c31ffd816003a92bbeb6
Harshil Agrawal <ghagrawal17@gmail.com>
2018-06-25 23:38:43 +0530

    Sign in page layout done (#15)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import SignUp from './components/SignUp';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/signup' component={SignUp} />
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
       </Switch>
     );
   }
