b74a863c8a6508f26fe0da00ea0b38b6179fc2a9
Harshil Agrawal <ghagrawal17@gmail.com>
2018-08-12 23:03:40 +0530

    .prod issue resolved

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyDT1J8D4p1mm0GHhTIeIDcgyNfwYByRkGk",
+  authDomain: "positivo-c0697.firebaseapp.com",
+  databaseURL: "https://positivo-c0697.firebaseio.com",
+  projectId: "positivo-c0697",
+  storageBucket: "positivo-c0697.appspot.com",
+  messagingSenderId: "727575854152"
+};
